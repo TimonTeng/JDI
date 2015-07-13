c60400df6eb382e435644a5534422691e539a47a
TimonTeng <d.penghai@gmail.com>
2015-07-13 23:29:33 +0800

    test
    test

---
+++ jdi/src/main/java/cn/teng/jdi/App.java
@@ -0,0 +1,13 @@
+package cn.teng.jdi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
